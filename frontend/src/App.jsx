c0160d71ffdc5249c2d0dd6e446ea20d38806a2c
prabha_g5 <238w1a12g5@vrsec.ac.in>
2026-04-18 13:47:19 +0530

    modified app.jsx to avoid merge conflicts

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,22 @@
-import MapView from './components/MapView';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import MapView from "./components/MapView";
+import SocketTest from "./components/socketTest";
 
 function App() {
-  return <MapView />;
+  return (
+    <Router>
+      <Routes>
+        {/* 🔥 Map page */}
+        <Route path="/map" element={<MapView />} />
+
+        {/* 🔧 Optional debug page */}
+        <Route path="/socket" element={<SocketTest />} />
+
+        {/* 🏠 Default route */}
+        <Route path="/" element={<div>Home Page</div>} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
